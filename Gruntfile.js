190fbed0611dd32b2105570efda235bdb222754a
Parul Sharma <psharma@brightcove.com>
2014-03-03 11:52:26 -0500

    changes5

--- Gruntfile.js
+++ Gruntfile.js
@@ -101,14 +101,15 @@ module.exports = function(grunt) {
   });
 
   // These plugins provide necessary tasks.
+  grunt.loadNpmTasks('grunt-karma');
+  grunt.loadNpmTasks('grunt-saucelabs');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-saucelabs');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-karma');
+  
 
   grunt.registerTask('manifests-to-js', 'Wrap the test fixtures and output' +
                      ' so they can be loaded in a browser',
--- test/karma.conf.js
+++ test/karma.conf.js
@@ -98,7 +98,7 @@ module.exports = function(config) {
     sauceLabs: {
       username: "sheff555",
       accessKey: "d04372cc-0fc3-4e59-aa02-3cfd9df03240",
-      startConnect: false,
+      startConnect: true,
       testName: 'ps test'
     },
 

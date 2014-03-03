440ad567242c41837420575aabfc2523de554c37
Parul Sharma <psharma@brightcove.com>
2014-03-03 10:16:50 -0500

    more changes2

--- Gruntfile.js
+++ Gruntfile.js
@@ -96,15 +96,7 @@ module.exports = function(grunt) {
         configFile: 'test/karma.conf.js',
         autoWatch: false
       }
-    },
-
-  // Loading dependencies
-  for (var key in grunt.file.readJSON("package.json").devDependencies) {
-    if (key !== "grunt" && key.indexOf("grunt") === 0) grunt.loadNpmTasks(key);
-  }
-
-  grunt.registerTask("dev", ["connect", "watch"]);
-
+    }
 
   });
 

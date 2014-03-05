365fed792ca7d0578d542b6e63bfa9684636c5f6
Parul Sharma <psharma@brightcove.com>
2014-03-05 15:58:49 -0500

    latest1

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -100,6 +100,7 @@ module.exports = function(config) {
       username: "sheff555",
       accessKey: "d04372cc-0fc3-4e59-aa02-3cfd9df03240",
       startConnect: true,
+      tunnelIdentifier: 'karma1394049905',
       testName: 'ps test sample'
     },
 
